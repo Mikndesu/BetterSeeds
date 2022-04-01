4b97ea9b2aab63d5f6f64df4fa7cd97fdfc66669
MitsukiGoto <mi22kknsinge@ezweb.ne.jp>
2022-04-01 13:29:48 +0900

    set collision detection as it seems

--- src/main/kotlin/com/github/mikn/better_seeds/blocks/MagicBreadStockBlock.kt
+++ src/main/kotlin/com/github/mikn/better_seeds/blocks/MagicBreadStockBlock.kt
@@ -1,35 +1,32 @@
 package com.github.mikn.better_seeds.blocks
 
 import com.github.mikn.better_seeds.blocks.entity.MagicBreadStockBlockEntity
-import com.github.mikn.better_seeds.blocks.entity.SeedModifierBlockEntity
 import com.github.mikn.better_seeds.container.MagicBreadBlockContainer
-import com.github.mikn.better_seeds.container.SeedModifierBlockContainer
-import com.github.mikn.better_seeds.init.BlockEntityInit
+import com.github.mikn.better_seeds.init.BlockInit
 import net.minecraft.core.BlockPos
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
-import net.minecraft.world.MenuProvider
-import net.minecraft.world.Nameable
 import net.minecraft.world.SimpleMenuProvider
-import net.minecraft.world.entity.LivingEntity
-import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.entity.player.Player
-import net.minecraft.world.inventory.ContainerLevelAccess
-import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.EntityBlock
+import net.minecraft.world.level.block.SlabBlock
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.BlockEntityTicker
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
-import net.minecraft.world.level.pathfinder.PathComputationType
+import net.minecraft.world.level.block.state.properties.SlabType
 import net.minecraft.world.phys.BlockHitResult
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.Shapes
+import net.minecraft.world.phys.shapes.VoxelShape
 import net.minecraftforge.network.NetworkHooks
 
 class MagicBreadStockBlock(properties:Properties): Block(properties), EntityBlock {
+
     override fun <T : BlockEntity?> getTicker(
         p_153212_: Level,
         p_153213_: BlockState,
@@ -57,5 +54,14 @@ class MagicBreadStockBlock(properties:Properties): Block(properties), EntityBloc
             NetworkHooks.openGui(p_60506_ as ServerPlayer, container, p_60505_);
         }
         return InteractionResult.SUCCESS
+    }
+
+    override fun getShape(
+        p_60555_: BlockState,
+        p_60556_: BlockGetter,
+        p_60557_: BlockPos,
+        p_60558_: CollisionContext
+    ): VoxelShape {
+        return box(4.0,0.0, 0.0, 12.0, 16.0, 16.0)
     }
 }
